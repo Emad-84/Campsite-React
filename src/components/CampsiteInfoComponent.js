f7d804d75a2c98e45639d78d81a5cafbbfa78dbd
Emad Adas <emad.adas@yahoo.com>
2021-06-04 15:01:39 -0700

    Fetch Post

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -53,7 +53,7 @@ class CommentForm extends React.Component {
     });
   }
   handleSubmit(values) {
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
   render() {
     return (
@@ -124,7 +124,7 @@ class CommentForm extends React.Component {
   }
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -144,7 +144,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </div>
           );
         })}
-        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
     );
   }
@@ -192,7 +192,7 @@ function CampsiteInfo(props) {
             <RenderCampsite campsite={props.campsite} />
             <RenderComments
               comments={props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               campsiteId={props.campsite.id}
             />
           </div>
@@ -202,3 +202,4 @@ function CampsiteInfo(props) {
   }
 }
 export default CampsiteInfo;
+
